2952fda6f1fa31962dbd03b27d08e49da72cf4d6
Joel Austin <Jta26@pitt.edu>
2017-02-06 22:28:01 -0500

    Logout Button Done

--- app/src/main/java/net/joelaustin/bablproject/MainActivity.java
+++ app/src/main/java/net/joelaustin/bablproject/MainActivity.java
@@ -1,17 +1,19 @@
 package net.joelaustin.bablproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    BABLDataLocal dataLocal = new BABLDataLocal();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BABLDataLocal dataLocal = new BABLDataLocal();
+
         TextView txvWelcome = (TextView) findViewById(R.id.txvWelcome);
 
         txvWelcome.setText(txvWelcome.getText() + " " + dataLocal.get_strFirstName());
@@ -19,4 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void btnLogoutOnClick(View v) {
+
+        dataLocal.set_strFirstName(null);
+        dataLocal.set_strUsername(null);
+        dataLocal.set_strLang1(null);
+        dataLocal.set_strLang2(null);
+        dataLocal.set_strLang3(null);
+        dataLocal.set_strLang4(null);
+        dataLocal.set_strLang5(null);
+        dataLocal.set_intCampusAttend(null);
+        dataLocal.set_boolMain(null);
+        dataLocal.set_boolJohnstown(null);
+        dataLocal.set_boolBradford(null);
+        dataLocal.set_boolTitusville(null);
+        dataLocal.set_boolGreensburg(null);
+
+        Intent intent = new Intent(getApplication().getBaseContext(), StartActivity.class);
+        startActivity(intent);
+
+
+    }
+
 }
